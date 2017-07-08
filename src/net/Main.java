31e09aa900100e2c15ba4f800fea04bb0b4ec470
Oksana <pasom@bigmir.net>
2017-07-09 02:06:15 +0300

    Min value

--- src/net/Main.java
+++ src/net/Main.java
@@ -3,6 +3,8 @@ package net;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MinValue minValue = new MinValue();
+        int[] mass = {0,5,6,7,2,-3,4,1,2};
+        System.out.println(minValue.minValue(mass));
     }
 }
---
+++ src/net/MinValue.java
@@ -0,0 +1,14 @@
+package net;
+
+public class MinValue {
+    public Integer minValue(int[] mass){
+        int min = mass[0];
+        for(int i=0; i<mass.length; i++){
+            if (mass[i]<min){
+                min = mass[i];
+            }
+
+        }
+        return min;
+    }
+}

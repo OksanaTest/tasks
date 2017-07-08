bd783341d5afe78c64a380418708267015990377
Oksana <pasom@bigmir.net>
2017-07-09 02:33:27 +0300

    Sorted Array

---
+++ src/net/IsArraySorted.java
@@ -0,0 +1,18 @@
+package net;
+
+public class IsArraySorted {
+    public boolean isArraySorted(int[] mass){
+        boolean k = false;
+        for (int i = 0; i<mass.length; i++ ){
+            int min = mass[i];
+            for (int j = i+1; j<mass.length; j++){
+                if (mass[j]<min){
+                    k = false;
+                    return k;
+                }else{k = true;}
+            }
+
+        }
+        return k;
+    }
+}
--- src/net/Main.java
+++ src/net/Main.java
@@ -3,8 +3,18 @@ package net;
 public class Main {
 
     public static void main(String[] args) {
-        MinValue minValue = new MinValue();
+
         int[] mass = {0,5,6,7,2,-3,4,1,2};
+
+        MinValue minValue = new MinValue();
         System.out.println(minValue.minValue(mass));
+
+        int[] mass1 = {-900, -899, -700, 4, 34, 45};
+
+        IsArraySorted isArraySorted = new IsArraySorted();
+        System.out.println(isArraySorted.isArraySorted(mass1));
+        System.out.println(isArraySorted.isArraySorted(mass));
+
+
     }
 }
